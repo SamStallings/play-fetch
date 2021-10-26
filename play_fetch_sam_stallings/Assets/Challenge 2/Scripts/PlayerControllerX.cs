3dfef522516313bdd5ff6bae890261da2ad3a453
Sam Stallings <10915865@my.uvu.edu>
2021-10-26 15:15:29 -0600

    finished prototype 2 challenges

--- play_fetch_sam_stallings/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ play_fetch_sam_stallings/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float coolDown;
+    private float timeToRespawn = 1.5f;
 
     // Update is called once per frame
     void Update()
     {
+        coolDown += Time.deltaTime;
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && coolDown > timeToRespawn)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            coolDown = 0;
         }
     }
 }
